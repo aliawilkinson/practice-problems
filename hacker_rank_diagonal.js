6f4d76aa002b3da7ed46fe706dd34d3c02cf86e3
Alia Wilkinson <aliawilkinson@gmail.com>
2018-06-25 16:59:46 -0700

    hacker practice

--- hacker_rank_diagonal.js
+++ hacker_rank_diagonal.js
@@ -8,3 +8,28 @@ function diagonalDifference(arr) {
     return Math.abs(leftDiag - rightDiag);
 
 }
+
+var sampleArray = [-4, 3, -9, 0, 4, 1]
+
+function plusMinus(arr) {
+    var negNums = 0;
+    var posNums = 0;
+    var zeros = 0;
+    var answers = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (Math.sign(arr[i]) === 1) {
+            posNums++;
+        }
+        else if (Math.sign(arr[i]) === -1) {
+            negNums++;
+        }
+        else if (Math.sign(arr[i]) === 0) {
+            zeros++
+        }
+    }
+    answers.push(posNums, negNums, zeros)
+    for (var x = 0; x < answers.length; x++) {
+        answers[x] = parseFloat((answers[x] / arr.length)).toFixed(6);
+    }
+    return answers;
+}
